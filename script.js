e902c8fabed5bc12d06fdd8b7c4bd4434acf157c
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-04-21 13:50:17 +0400

    Add a function that plays a single round

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ function getComputerChoice() {
     }
 }
 
-function getHumanCoice() {
-    let choice = prompt("Rock, Paper or scissors?", '');
-    return choice.toLowerCase();
-}
-
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    function getHumanCoice() {
+    let choice = prompt("Rock, Paper or scissors?", '');
+    return choice.toLowerCase();
+}
+    humanChoice = getHumanCoice().toLowerCase();
+    computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+        return 'Tie'
+    } else if (humanChoice === 'rock' && computerChoice === 'paper'
+        || humanChoice === 'paper' && computerChoice === 'scissors'
+        || humanChoice === 'scissors' && computerChoice === 'rock') {
+            ++computerScore;
+            return 'You lost this time...'
+        } else if (humanChoice === 'rock' && computerChoice === 'scissors'
+            || humanChoice === 'scissors' && computerChoice === 'paper' 
+            || humanChoice === 'paper' && computerChoice === 'rock'
+        ) {
+            ++humanScore;
+            return 'You win the round!'
+        }
+}
+console.log(playRound());
