08d774bccb78609e14e43297a5cda2900c46e0cc
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-07-20 09:56:35 +0400

    Correct the human choice that always was only rock

--- script.js
+++ script.js
@@ -18,38 +18,34 @@ console.log(`Computer's score is ${computerScore}`);
 let rock = document.getElementById("rock");
 let scissors = document.getElementById("scissors");
 let paper = document.getElementById("paper");
-
-function getHumanCoice(choice) {
-    if (rock) {
-        choice = 'rock';
-    } else if (paper) {
-        choice = 'paper';
-    } else {choice = 'scissors'};
-    return choice;
-}
+let displayResult = document.getElementById("display-result");
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanCoice();
     computerChoice = getComputerChoice();
     if (humanChoice === computerChoice) {
-        console.log('Tie');
-        console.log(`Your score is ${humanScore}`);
-        console.log(`Computer score is ${computerScore}`);
+        displayResult.textContent = `Tie! Your score is ${humanScore}. Computer score is ${computerScore}.`;
+        console.log(humanChoice, computerChoice);
     } else if (humanChoice === 'rock' && computerChoice === 'paper'
         || humanChoice === 'paper' && computerChoice === 'scissors'
         || humanChoice === 'scissors' && computerChoice === 'rock') {
-            console.log(`You lost this time, ${computerChoice} beats ${humanChoice}.`);
-            console.log(`Your score is ${humanScore}`);
-            console.log(`Computer score is ${++computerScore}.`);
+            console.log(humanChoice, computerChoice);
+            displayResult.textContent = `You lost this time, ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. Computer score is ${++computerScore}.`
         } else if (humanChoice === 'rock' && computerChoice === 'scissors'
             || humanChoice === 'scissors' && computerChoice === 'paper' 
             || humanChoice === 'paper' && computerChoice === 'rock'
         ) {
-            console.log(`You win the round, ${humanChoice} beats ${computerChoice}!`);
-            console.log(`Your score is ${++humanScore}`);
-            console.log(`Computer score is ${computerScore}`);
+            console.log(humanChoice, computerChoice);
+            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}! Your score is ${++humanScore}. Computer score is ${computerScore}.`
         }
 }
-rock.addEventListener("click", playRound);
-paper.addEventListener("click", playRound);
-scissors.addEventListener("click", playRound);
+rock.addEventListener("click", () => {
+    playRound('rock', getComputerChoice);
+});
+paper.addEventListener("click", () => {
+    playRound('paper', getComputerChoice);
+});
+scissors.addEventListener("click", () => {
+    playRound('scissors', getComputerChoice);
+});
+
+//Make a function that makes a game play until one the of players get 5 points.
