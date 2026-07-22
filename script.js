36ad20f4e63a8fb3331429cd9e4a834a722f0f6f
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-07-22 08:15:48 +0400

    Remove all other unnecessary console log messages

--- script.js
+++ script.js
@@ -8,12 +8,8 @@ function getComputerChoice() {
         return 'scissors'
     }
 }
-
 let humanScore = 0;
 let computerScore = 0;
-
-console.log(`Your score is ${humanScore}`);
-console.log(`Computer's score is ${computerScore}`);
 
 let rock = document.getElementById("rock");
 let scissors = document.getElementById("scissors");
@@ -21,23 +17,24 @@ let paper = document.getElementById("paper");
 let displayResult = document.getElementById("display-result");
 
 function playRound(humanChoice, computerChoice) {
+    
     computerChoice = getComputerChoice();
     if (humanChoice === computerChoice) {
         displayResult.textContent = `Tie! Your score is ${humanScore}. Computer score is ${computerScore}.`;
-        console.log(humanChoice, computerChoice);
     } else if (humanChoice === 'rock' && computerChoice === 'paper'
         || humanChoice === 'paper' && computerChoice === 'scissors'
         || humanChoice === 'scissors' && computerChoice === 'rock') {
-            console.log(humanChoice, computerChoice);
-            displayResult.textContent = `You lost this time, ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. Computer score is ${++computerScore}.`
+            ++computerScore;
+            displayResult.textContent = `You lost this time, ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. Computer score is ${computerScore}.`
         } else if (humanChoice === 'rock' && computerChoice === 'scissors'
             || humanChoice === 'scissors' && computerChoice === 'paper' 
             || humanChoice === 'paper' && computerChoice === 'rock'
         ) {
-            console.log(humanChoice, computerChoice);
-            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}! Your score is ${++humanScore}. Computer score is ${computerScore}.`
+            ++humanScore;
+            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}! Your score is ${humanScore}. Computer score is ${computerScore}.`
         }
-}
+    } 
+
 rock.addEventListener("click", () => {
     playRound('rock', getComputerChoice);
 });
@@ -47,5 +44,4 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
     playRound('scissors', getComputerChoice);
 });
-
 //Make a function that makes a game play until one the of players get 5 points. Remove all unnecessary console messages.
