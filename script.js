d283262af624cd9bfc5fb4877d8032d600ca4199
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-07-23 22:08:20 +0400

    Add separate divs for scores of players

--- script.js
+++ script.js
@@ -14,23 +14,35 @@ let computerScore = 0;
 let rock = document.getElementById("rock");
 let scissors = document.getElementById("scissors");
 let paper = document.getElementById("paper");
+
 let displayResult = document.getElementById("display-result");
-displayResult.textContent = 'First who reaches 5 points win the game';
+displayResult.textContent = 'First who reaches 5 points wins the game';
+let displayHumanScore = document.getElementById('display-human-score');
+let displayCompScore = document.getElementById('display-comp-score');
+displayHumanScore.textContent = 'Your score:';
+displayCompScore.textContent = 'Computer score:';
+
 function playRound(humanChoice) {
     computerChoice = getComputerChoice();
     if (humanChoice === computerChoice) {
-        displayResult.textContent = `Tie! Your score is ${humanScore}. Computer score is ${computerScore}.`;
+        displayResult.textContent = `Tie!`;
+        displayHumanScore.textContent = `Your score: ${humanScore}`;
+        displayCompScore.textContent = `Computer score: ${computerScore}`;
     } else if (humanChoice === 'rock' && computerChoice === 'paper'
         || humanChoice === 'paper' && computerChoice === 'scissors'
         || humanChoice === 'scissors' && computerChoice === 'rock') {
-            ++computerScore;
-            displayResult.textContent = `You lost this time, ${computerChoice} beats ${humanChoice}. Your score is ${humanScore}. Computer score is ${computerScore}.`
+            ++computerScore;    
+            displayResult.textContent = 'You lose this time.';    
+            displayHumanScore.textContent = `Your score: ${humanScore}`;
+            displayCompScore.textContent = `Computer score: ${computerScore}`;
         } else if (humanChoice === 'rock' && computerChoice === 'scissors'
             || humanChoice === 'scissors' && computerChoice === 'paper' 
             || humanChoice === 'paper' && computerChoice === 'rock'
         ) {
             ++humanScore;
-            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}! Your score is ${humanScore}. Computer score is ${computerScore}.`
+            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}!`
+            displayHumanScore.textContent = `Your score: ${humanScore}`;
+            displayCompScore.textContent = `Computer score: ${computerScore}`;
         }
         if (humanScore === 5) {
             displayResult.textContent = 'Congratulations, you win the game!'
