1edb765a67887f430dbd37da4f7646781878cda9
Aydin Gurbanov <qurbanovaydin423@gmail.com>
2026-09-07 18:28:00 +0400

    Add ukrainian language for the game

--- rps-ua/script-ua.js
+++ rps-ua/script-ua.js
@@ -14,22 +14,22 @@ let humanWinnings = 0;
 let computerWinnings = 0;
 
 let rock = document.createElement("button");
-rock.textContent = 'ROCK';
+rock.textContent = 'КАМІНЬ';
 rock.setAttribute('id', 'rock');
 rock.classList.add('playing-buttons');
 
 let scissors = document.createElement("button");
-scissors.textContent = 'SCISSORS';
+scissors.textContent = 'НОЖИЦІ';
 scissors.setAttribute('id', 'scissors');
 scissors.classList.add('playing-buttons');
 
 let paper = document.createElement("button");
-paper.textContent = 'PAPER';
+paper.textContent = 'ПАПІР';
 paper.setAttribute('id', 'paper');
 paper.classList.add('playing-buttons');
 
 let restartWinnings = document.createElement('button');
-restartWinnings.textContent = 'RESTART WINNINGS';
+restartWinnings.textContent = 'СКИНУТИ ПЕРЕМОГИ';
 restartWinnings.setAttribute("id", "restart-winnings");
 restartWinnings.style.backgroundColor = 'rgb(71, 22, 22)';
 
@@ -55,7 +55,7 @@ let winningsBox = document.getElementById("winnings-box");
 let displayHumanWinnings = document.getElementById("human-winnings");
 let displayComputerWinnings = document.getElementById('computer-winnings');
 let startStrong = document.createElement("strong");
-startStrong.textContent = 'START';
+startStrong.textContent = 'СТАРТ';
 let dot = document.createElement('span');
 dot.textContent = '.';
 
@@ -64,18 +64,18 @@ let figure = document.querySelector("figure");
 let englishLanguage = document.createElement("a");
 let listOfLanguages = document.createElement("list");
 listOfLanguages.setAttribute("id", "list-of-languages");
-englishLanguage.href = "index.html"
+englishLanguage.href = "../index.html"
 englishLanguage.classList.add("language");
 let englishFlag = document.createElement("img");
-englishFlag.src = "images/gb.svg";
+englishFlag.src = "../images/gb.svg";
 englishFlag.classList.add("flags");
 let englishCaption = document.createElement("p");
 englishCaption.textContent = "English";
 let russianLanguage = document.createElement("a");
-russianLanguage.href = "rps-ru/index-ru.html"
+russianLanguage.href = "../rps-ru/index-ru.html"
 russianLanguage.classList.add("language");
 let russianFlag = document.createElement("img");
-russianFlag.src = "images/ru.svg";
+russianFlag.src = "../images/ru.svg";
 russianFlag.classList.add("flags");
 let russianCaption = document.createElement("p");
 russianCaption.textContent = "Русский";
@@ -84,7 +84,7 @@ chooseLanguage.addEventListener("click", e => {
     startGame.remove();
     winningsBox.remove();
     figure.remove();
-    instruction.textContent = "Choose a language.";
+    instruction.textContent = "Обрати мову.";
     main.appendChild(listOfLanguages);
     listOfLanguages.appendChild(englishLanguage);
     englishLanguage.appendChild(englishFlag);
@@ -97,27 +97,27 @@ chooseLanguage.addEventListener("click", e => {
 function playRound(humanChoice) {
     computerChoice = getComputerChoice();
     if (humanChoice === computerChoice) {
-        displayResult.textContent = `Tie!`;
-        displayHumanScore.textContent = `Your score: ${humanScore}`;
-        displayCompScore.textContent = `Computer score: ${computerScore}`;
+        displayResult.textContent = `Нічия!`;
+        displayHumanScore.textContent = `Ваши очки: ${humanScore}`;
+        displayCompScore.textContent = `Очки ШІ: ${computerScore}`;
     } else if (humanChoice === 'rock' && computerChoice === 'paper'
         || humanChoice === 'paper' && computerChoice === 'scissors'
         || humanChoice === 'scissors' && computerChoice === 'rock') {
             ++computerScore;    
-            displayResult.textContent = `You lose this time, ${computerChoice} beats ${humanChoice}.`;    
-            displayHumanScore.textContent = `Your score: ${humanScore}`;
-            displayCompScore.textContent = `Computer score: ${computerScore}`;
+            displayResult.textContent = `Цього разу вам не пощастило: ваш вибір: ${computerChoice}, вибір ШІ: ${humanChoice}.`;    
+            displayHumanScore.textContent = `Ваши очки: ${humanScore}`;
+            displayCompScore.textContent = `Очки ШІ: ${computerScore}`;
         } else if (humanChoice === 'rock' && computerChoice === 'scissors'
             || humanChoice === 'scissors' && computerChoice === 'paper' 
             || humanChoice === 'paper' && computerChoice === 'rock'
         ) {
             ++humanScore;
-            displayResult.textContent = `You win the round, ${humanChoice} beats ${computerChoice}!`
-            displayHumanScore.textContent = `Your score: ${humanScore}`;
-            displayCompScore.textContent = `Computer score: ${computerScore}`;
+            displayResult.textContent = `Ви виграли цей раунд, ваш вибір: ${humanChoice}, вибір ШІ: ${computerChoice}!`
+            displayHumanScore.textContent = `Ваши очки: ${humanScore}`;
+            displayCompScore.textContent = `Очки ШІ: ${computerScore}`;
         }
         if (humanScore === 5) {
-            displayResult.textContent = 'Congratulations, you win the game!'
+            displayResult.textContent = 'Вітаемо, ви виграли гру!'
             instruction.after(startGame);
             rock.remove();
             paper.remove();
@@ -127,17 +127,17 @@ function playRound(humanChoice) {
             displayHumanScore.remove();
             displayCompScore.remove();
             ++humanWinnings;
-            displayHumanWinnings.textContent = `Your winnings: ${humanWinnings}`;
-            displayComputerWinnings.textContent = `Computer winnings: ${computerWinnings}`;
+            displayHumanWinnings.textContent = `Ваши перемоги: ${humanWinnings}`;
+            displayComputerWinnings.textContent = `Перемоги ШІ: ${computerWinnings}`;
             winningsBox.appendChild(displayHumanWinnings);
             winningsBox.appendChild(displayComputerWinnings);
             main.appendChild(restartWinnings);
             restartWinnings.style.backgroundColor = 'rgb(71, 22, 22)'
-            instruction.textContent = `To start a new game press `;
+            instruction.textContent = `Щоб почати гру, натисніть `;
             instruction.appendChild(startStrong);
             instruction.appendChild(dot);
         } else if (computerScore === 5) {
-            displayResult.textContent = 'You lose the game. Try again.'
+            displayResult.textContent = 'На жаль, ви програли. Спробуйте ще раз.'
             instruction.after(startGame);
             rock.remove();
             paper.remove();
@@ -147,13 +147,13 @@ function playRound(humanChoice) {
             displayHumanScore.remove();
             displayCompScore.remove();
             ++computerWinnings;
-            displayHumanWinnings.textContent = `Your winnings: ${humanWinnings}`;
-            displayComputerWinnings.textContent = `Computer winnings: ${computerWinnings}`;
+            displayHumanWinnings.textContent = `Ваши перемоги: ${humanWinnings}`;
+            displayComputerWinnings.textContent = `Перемоги ШІ: ${computerWinnings}`;
             winningsBox.appendChild(displayHumanWinnings);
             winningsBox.appendChild(displayComputerWinnings);
             main.appendChild(restartWinnings);
             restartWinnings.style.backgroundColor = 'rgb(71, 22, 22)'
-            instruction.textContent = `To start a new game press `;
+            instruction.textContent = `Щоб почати гру, натисніть `;
             instruction.appendChild(startStrong);
             instruction.appendChild(dot);
         }
@@ -165,10 +165,10 @@ let scores = document.createElement('div');
 scores.setAttribute('id', 'scores');
 
 startGame.addEventListener("click", e => {
-    displayResult.textContent = 'Choose the option.'
-    displayHumanScore.textContent = 'Your score: 0';
-    displayCompScore.textContent = 'Computer score: 0';
-    instruction.textContent = 'First who reaches 5 points wins the game!'
+    displayResult.textContent = 'Обрати предмет.'
+    displayHumanScore.textContent = 'Ваши очки: 0';
+    displayCompScore.textContent = 'Очки ШІ: 0';
+    instruction.textContent = 'Перший, хто набрав 5 очків, виграв раунд!'
     rps.appendChild(rock);
     rps.appendChild(paper);
     rps.appendChild(scissors);
@@ -201,19 +201,19 @@ restartWinnings.addEventListener("click", e => {
     displayComputerWinnings.remove();
     e.target.remove();
     let areYouSureQuestion = document.createElement('p');
-    areYouSureQuestion.textContent = "Are you sure you want to remove all the winnings?"
+    areYouSureQuestion.textContent = "Ви впевнині, що хочете сбросити усі перемоги? Повернення буде неможливо."
     main.appendChild(areYouSureQuestion);
     let yesButton = document.createElement("button");
-    yesButton.textContent = "Yes";
+    yesButton.textContent = "Так";
     let noButton = document.createElement('button');
-    noButton.textContent = "No";
+    noButton.textContent = "Ні";
     main.appendChild(yesButton);
     main.appendChild(noButton);
     yesButton.addEventListener("click", e => {
         humanWinnings = 0;
         computerWinnings = 0;
-        displayHumanWinnings.textContent = `Your winnings: ${humanWinnings}`;
-        displayComputerWinnings.textContent = `Computer winnings: ${computerWinnings}`;
+        displayHumanWinnings.textContent = `Ваши перемоги: ${humanWinnings}`;
+        displayComputerWinnings.textContent = `Перемоги ШІ: ${computerWinnings}`;
         e.target.remove();
         noButton.remove();
         areYouSureQuestion.remove();
@@ -225,8 +225,8 @@ restartWinnings.addEventListener("click", e => {
         main.appendChild(chooseLanguage);
     });
     noButton.addEventListener("click", e => {
-        displayHumanWinnings.textContent = `Your winnings: ${humanWinnings}`;
-        displayComputerWinnings.textContent = `Computer winnings: ${computerWinnings}`;
+        displayHumanWinnings.textContent = `Ваши перемоги: ${humanWinnings}`;
+        displayComputerWinnings.textContent = `Перемоги ШІ: ${computerWinnings}`;
         yesButton.remove();
         e.target.remove();
         areYouSureQuestion.remove();
